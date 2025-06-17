e190241994c936ab6b02c6dfafebf2a0c9c5f171
unish <unish@innovatetech.co>
2025-06-17 06:19:27 +0545

    [update] refactor minor changes

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,4 +1,8 @@
-import { LayoutModule, BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import {
+  LayoutModule,
+  BreakpointObserver,
+  Breakpoints,
+} from '@angular/cdk/layout';
 import { Component, inject, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -6,23 +10,30 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     CommonModule,
     LayoutModule,
-    MatSidenavModule,
     MatToolbarModule,
-    MatIconModule,
-    MatListModule,
+    MatButtonModule,
     MatCardModule,
-    MatGridListModule,],
+    MatIconModule,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
-  standalone:true,
+  standalone: true,
 })
 export class Dashboard {
-
+  features = [
+    { title: 'Fast Setup', desc: 'Deploy your site in minutes with Angular.' },
+    { title: 'Modern Design', desc: 'Built with Angular Material and SCSS.' },
+    {
+      title: 'Standalone App',
+      desc: 'Powered by Angular 20 standalone components.',
+    },
+  ];
 }
